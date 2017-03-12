9bf620850dc67d490556675b88c584aa6f528559
Squidly271 <azawadzki@arrowgames.com>
2017-03-11 21:15:10 -0500

    Add icon, fixes

--- source/ca.turbo/usr/local/emhttp/plugins/ca.turbo/include/exec.php
+++ source/ca.turbo/usr/local/emhttp/plugins/ca.turbo/include/exec.php
@@ -29,18 +29,12 @@ switch ($_POST['action']) {
         @unlink($turboPaths['backgroundPID']);
       }
       logger("Starting Auto Turbo");
-      sleep(5);
-#      exec("/usr/local/emhttp/plugins/ca.turbo/scripts/startBackground.sh  & > /dev/null | at NOW -M >/dev/null 2>&1");
-$descriptorspec = array(
-   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
-);
-proc_open("/usr/local/emhttp/plugins/ca.turbo/scripts/auto_turbo.php",$descriptorspec,$pipes);
-
-
-
-
+      $descriptorspec = array(
+        0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+        1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+        2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+      );
+      proc_open("/usr/local/emhttp/plugins/ca.turbo/scripts/auto_turbo.php",$descriptorspec,$pipes);
     }        
     echo "Settings Updated";
     break;
